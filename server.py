1df437f9d677346ebebe18db54606392fa14f51e
Emmanuel26 <aarteaga96@gmail.com>
2021-08-17 16:32:32 -0500

    Update server.py

--- server.py
+++ server.py
@@ -9,40 +9,34 @@ sock.bind(('127.0.0.1', 4001))
 sock.listen(5)
 
 
-def listToString(s):
-    # initialize an empty string
-    str1 = " -> "
-
-    # traverse in the string
-    for ele in s:
-        str1 += ele
-
-        # return string
-    return str1
-
 def handle_client(clientsocket):
     print("Connection is stablished", address)
-    data=clientsocket.recv(4096)
-    data_variable=pickle.loads(data)
+    data = clientsocket.recv(4096)
+    data_variable = pickle.loads(data)
 
-    G=grafo()
-    shortestPath(G,data_variable[0])
-    vec= printPath(G, data_variable[1])
-    
-    
-    
-    msg = pickle.dumps(vec)
-    
-    clientsocket.send(msg)
-    clientsocket.close()
-    
-    
-    
+    G = grafo()
+    shortestPath(G, data_variable[0])
+    vec = printPath(G, data_variable[1])
+
+    Vertices = []
+    Ubicacion = []
+
+    for i in vec:
+        Vertices.append(('Lugar', str(i.id)))
+        Vertices.append(('Latitud', str(i.la)))
+        Vertices.append(('Longitud', str(i.lo)))
+        Ubicacion.append(dict(Vertices))
+        Vertices.clear()
+
     print("Algoritmo completado enviando a", address)
-    
-    
 
-  
+    msg = pickle.dumps(Ubicacion)
+
+    clientsocket.send(msg)
+    print(Ubicacion)
+    clientsocket.close()
+
+
 try:
     while True:
         clientsocket, address = sock.accept()
@@ -51,5 +45,5 @@ try:
 
 
 except KeyboardInterrupt:
-    print ("Finishing. . .")
+    print("Finishing. . .")
 sock.close()
