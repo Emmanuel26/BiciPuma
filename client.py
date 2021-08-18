d0574dfa872ed9a69c6508be7a2ca1a1123e46cf
Emmanuel26 <aarteaga96@gmail.com>
2021-08-18 16:36:37 -0500

    Update client.py

--- client.py
+++ client.py
@@ -2,14 +2,54 @@ import socket
 import pickle
 from Mapa import *
 
-HOST = '127.0.0.1'
+HOST = '192.168.0.6'
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST,4001))
 
-inicio = input("Tu ubicacion actual ")
-dest = input("Tu destino ")
-datosin=[inicio,dest]
-data_string=pickle.dumps(datosin)
+print (" Calcular Ruta Bicipuma ")
+
+print ("1. Anexo de Filosofía y Letras")
+print ("2. Estadio")
+print ("3. Arquitectura")
+print ("4. Filosofía y Letras")
+print ("5. Ingeniería")
+print ("6. Derecho")
+print ("7. Medicina")
+print ("8. Química")
+print ("9. Anexo de Ingeniería")
+print ("10. Estadio Tapatío Méndez")
+print ("11. Ciencias")
+print ("12. Planta Alta")
+print ("13. Planta Baja")
+print ("14. Ciencias Políticas")
+
+Ubicaciones=["Anexo de Filosofía y Letras","Estadio", "Arquitectura", "Filosofía y Letras","Ingeniería",
+     "Derecho","Medicina","Química","Anexo de Ingeniería","Estadio Tapatío Méndez", "Ciencias",
+     "Metro Universidad Planta Alta","Metro Universidad Planta Baja","Ciencias Políticas"]
+UbiIndex=["A","B","C","D","E","F","G","H","I","J","K","L","M","N"]
+
+while True:
+
+    try:
+
+        Inicio = int(input("Ingresa tu ubicacion "))
+        print("Haz seleccionado: "+str(Inicio)+") "+ Ubicaciones[Inicio-1] )
+        print("")
+        Destino = int(input("Ingresa tu destino "))
+        print("Haz seleccionado: " + str(Destino)+") "+Ubicaciones[Destino - 1])
+        print("")
+        Ubi = [Ubicaciones[Inicio-1], Ubicaciones[Destino-1]]
+        break
+    except IndexError:
+        print("introduce una ubicacion que exista en la ruta")
+    except ValueError:
+        print("introduce caracteres validos")
+
+
+Ubi2=[UbiIndex[Inicio-1],UbiIndex[Destino-1]]
+
+print("Calculando...")
+data_string=pickle.dumps(Ubi2)
 sock.send(data_string)
 
 
@@ -20,15 +60,8 @@ while True:
     if not msg:
         break   
 vecf = pickle.loads(b"".join(data))
+
 plotMapa(vecf)
-
-
-mapa=[]
-for i in vecf:
-    mapa.append(i.id)
-
-print (mapa)
-
-
+print("Tu ruta ha sido calulada")
 
 
